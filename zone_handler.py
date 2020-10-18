8681897e942c2eb28a221eb527df0e44e572ef7a
Zaqueu C <zaqueudovale@gmail.com>
2020-10-18 20:23:34 -0300

    Renomeando atributos e métodos.

--- zone_handler.py
+++ zone_handler.py
@@ -3,22 +3,30 @@ from zone import Zone
 
 class ZoneHandler:
     def __init__(self):
-        self.nodes = []
-        self.edges = {}
+        self.zones = []
+        self.ways = {}
 
-    def addNode(self, zone):
-        self.nodes.append(zone)
+    def add(self, zone):
+        self.zones.append(zone)
 
-    def addEdge(self, zone1, zone2):
-        if zone1.key not in self.edges:
-            self.edges[zone1.key] = [zone2]
+    def addListOf(self, zones):
+        for zone in zones:
+            self.add(zone)
+
+    def connectUnidirectionally(self, zone1, zone2):
+        if zone1.key not in self.ways:
+            self.ways[zone1.key] = [zone2]
         else:
-            self.edges[zone1.key].append(zone2)
+            self.ways[zone1.key].append(zone2)
+
+    def connect(self, zone1, zone2):
+        self.connectUnidirectionally(zone1, zone2)
+        self.connectUnidirectionally(zone2, zone1)
 
     def getZone(self, key):
-        return self.nodes[key]
+        return self.zones[key]
 
     def printGraph(self):
-        for key, list_values in self.edges.items():
+        for key, list_values in self.ways.items():
             for value in list_values:
                 print(key, value.key)
