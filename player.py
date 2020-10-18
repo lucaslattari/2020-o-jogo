7f11ad9c3a6e039cd86bd95e5d4cf2561d127e0f
Zaqueu C <zaqueudovale@gmail.com>
2020-10-18 20:22:53 -0300

    Adicionando alguns métodos fundamentais ao Player.

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 class Player:
     def __init__(self):
-        self.name = input("Informe o nome do jogador ou jogadora: ")
+        self.name = input("Informe o nome do jogador(a): ")
         self.level = 1
         self.hp = 100
         self.mp = 0
@@ -11,3 +11,21 @@ class Player:
 
     def getLocation(self):
         return self.location
+
+    def levelUp(self):
+        self.level += 1
+
+    def damage(self):
+        self.hp -= 10
+        if (self.hp < 0):
+            self.die()
+
+    def die(self):
+        self.hp = 0
+        print("WASTED")
+
+    # def pickUpItem(self, item):
+
+    # def showItems(self):
+
+    # def dropItem(self, item):
